e1fc2b2362dbf9f719be54e23f652c9ead1694b8
BoisonChang <dragoncres@gmail.com>
2023-02-28 00:20:16 +0800

    fix: fix typo in before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#downlad node and npm
-curl -o- 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install node 
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
-# create our working directory if it dosent exist
-
-DIR='/home/ec2-user/express-app'
-if [ -d "$DIR"]; then
-    echo "${DIR} exists"
-else 
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
